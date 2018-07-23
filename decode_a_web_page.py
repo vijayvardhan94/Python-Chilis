91ee4e3260aa4c4e2a03e8672f56fc73814db9ed
VIJAY TADIMETI <vijayvardhan.94@gmail.com>
2018-07-23 17:24:20 +0530

    adding files

--- decode_a_web_page.py
+++ decode_a_web_page.py
@@ -1,4 +1,18 @@
+#gathering headlines from ny times hompage using requests and beautiful soup
+
 import requests
-url = 'http://github.com'
+from bs4 import BeautifulSoup
+
+url = "http://www.nytimes.com"
+
+#get request
 r = requests.get(url)
-r_html = r.text
+
+#using soup
+
+soup = BeautifulSoup(r.text)
+for heading in soup.find_all(class_="story-heading"): 
+    if heading.a: 
+        print(heading.a.text.replace("\n", " ").strip())
+    else: 
+        print(heading.contents[0].strip())
